c28edf1c57c335148b21de5f63fc6f9cc594217d
Mike Kao <github@47north.lat>
2023-12-16 03:58:50 +0000

    install

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ YOUR_APP_NAME="langiot"
 REPO_URL="https://github.com/lenaxia/langiot.git"
 APP_DIR="/home/pi/$YOUR_APP_NAME"
 CONFIG_DIR="/etc/$YOUR_APP_NAME"
-LOG_FILE="/var/log/$YOUR_APP_NAME-install.log"
+LOG_FILE="~/$YOUR_APP_NAME-install.log"
 SERVICE_FILE="/etc/systemd/system/$YOUR_APP_NAME.service"
 
 # Function to log messages
@@ -13,43 +13,24 @@ log_message() {
     echo "$(date): $1" | tee -a "$LOG_FILE"
 }
 
-# Update and install dependencies
-log_message "Updating system and installing dependencies..."
-sudo apt-get update && sudo apt-get install -y git nodejs npm gcc libglib2.0-0 make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-if [ $? -ne 0 ]; then
-    log_message "Failed to install required packages."
-    exit 1
-fi
+# Ensure the log file exists
+touch "$LOG_FILE"
 
-# Install pyenv for Python version management
-log_message "Installing pyenv..."
-curl https://pyenv.run | bash
-export PATH="$HOME/.pyenv/bin:$PATH"
-eval "$(pyenv init --path)"
-eval "$(pyenv init -)"
-if [ $? -ne 0 ]; then
-    log_message "Failed to install pyenv."
-    exit 1
-fi
-
-# Check Python version and install new version if needed
-PYTHON_REQUIRED="3.9.0"
-pyenv install -s $PYTHON_REQUIRED
-pyenv global $PYTHON_REQUIRED
-if [ $? -ne 0 ]; then
-    log_message "Failed to install Python $PYTHON_REQUIRED."
-    exit 1
-fi
-
-# Reload shell
-exec "$SHELL"
-
+log_message "Starting installation script..."
 
 # Update and install dependencies
 log_message "Updating system and installing dependencies..."
 sudo apt-get update && sudo apt-get install -y git python3 python3-pip nodejs npm gcc libglib2.0-0
 if [ $? -ne 0 ]; then
     log_message "Failed to install required packages."
+    exit 1
+fi
+
+# Check Python version
+PYTHON_VERSION=$(python3 --version | cut -d " " -f 2)
+PYTHON_REQUIRED="3.9"
+if [[ "$PYTHON_VERSION" < "$PYTHON_REQUIRED" ]]; then
+    log_message "Python version $PYTHON_VERSION is not sufficient. Required version is $PYTHON_REQUIRED or higher."
     exit 1
 fi
 
@@ -104,11 +85,11 @@ Description=Gunicorn instance to serve Flask Application
 After=network.target
 
 [Service]
-User=pi
+User=yin
 WorkingDirectory=$APP_DIR/backend
 Environment=PYTHONDONTWRITEBYTECODE=1
 Environment=PYTHONUNBUFFERED=1
-ExecStart=/usr/bin/gunicorn --workers 3 --bind 0.0.0.0:80 'langiot:create_app()'
+ExecStart=/usr/bin/gunicorn --workers 3 --bind 0.0.0.0:80 'langiot'
 
 [Install]
 WantedBy=multi-user.target
@@ -121,10 +102,4 @@ fi
 
 # Enable and start the service
 log_message "Enabling and starting the service..."
-sudo systemctl enable $YOUR_APP_NAME.service && sudo systemctl start $YOUR_APP_NAME.service
-if [ $? -ne 0 ]; then
-    log_message "Failed to enable or start the service."
-    exit 1
-fi
-
-log_message "Installation of $YOUR_APP_NAME completed successfully."
+sudo systemctl enable $
